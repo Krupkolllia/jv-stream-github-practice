607a158a267a7298547dfa10d0c73f6a78c1d1fa
Krupkolllia <krupkoilla397@gmail.com>
2026-03-13 04:50:40 +0100

    feature: implement CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final int REQUIRED_AGE = 35;
+    public static final String REQUIRED_NATIONALITY = "Ukrainian";
+    public static final int REQUIRED_YEARS_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periods = candidate.getPeriodsInUkr().split("-");
+        int yearsInUkraine = Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]);
+
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= REQUIRED_AGE
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && yearsInUkraine >= REQUIRED_YEARS_IN_UKRAINE;
+    }
 }
